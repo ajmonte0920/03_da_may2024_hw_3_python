a67327561c3d70834cdfbc5b77494a9b20129628
ajmonte0920 <ajmonte0920@gmail.com>
2024-05-11 00:14:43 -0500

    03_da_may2024_hw_3_python

--- PyPoll/main_AJM.py
+++ PyPoll/main_AJM.py
@@ -22,8 +22,16 @@ with open(csvpath) as csvfile:
     csv_header = next(csvreader)
     print(f"CSV Header: {csv_header}")
 
-    # read each row of data after the header 
+    # read each row of data after the header
     for row in csvreader:
-            print(row)
+        # total vote cast
+        vote_count +=1
 
-
+         # list of candidates that received votes
+        row_candidate = row[2]
+        if row_candidate in candidate_dict.keys():
+            candidate_dict[row_candidate] += 1
+        else:
+            candidate_dict[row_candidate] = 1
+print(vote_count)
+print(candidate_dict)
