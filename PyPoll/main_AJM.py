55b93f7deb4b39c0218504f7194e648b3f80f398
ajmonte0920 <ajmonte0920@gmail.com>
2024-05-10 23:52:25 -0500

    03_da_may2024_hw_3_python

---
+++ PyPoll/main_AJM.py
@@ -0,0 +1,16 @@
+ # import os module 
+import os
+
+# import csv file module
+import csv
+
+# csv path 
+csvpath = "PyPoll/Resources/election_data.csv"
+
+# list the variables
+vote_count = 0
+candidate_dict = {}
+
+
+
+
